f2426d3f6c7440995a52e12e3bfec5f2f5850403
Arek Ruta <arek.ruta89@gmail.com>
2016-07-10 17:57:59 +0200

    dodanie loadDetails
    dodanie loadDetails i wyswietlenie w consoli objektu uzytkownika na
    ktorego kliknelismy user-details

--- app/pages/user-details/user-details.ts
+++ app/pages/user-details/user-details.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+// Import GithubUsers Provider
+import {GithubUsers} from '../../providers/github-users/github-users';
+
+// Import the User model
+import {User} from '../../models/user';
+
 @Component({
   templateUrl: 'build/pages/user-details/user-details.html',
+  
+  //Add GithubUsers provider
+  providers: [GithubUsers]
 })
 export class UserDetailsPage {
+	user: User = new User;
 	login: string;
 
-  constructor(public nav: NavController, navParams: NavParams) {
+  constructor(public nav: NavController, navParams: NavParams, githubUsers: GithubUsers) {
 	// Retrieve the login from the navigation parameters
     this.login = navParams.get('login');
+	
+	// Get the user details and log
+	githubUsers.loadDetails(this.login)
+		.then(user => console.log(user))
   }
 }
--- app/providers/github-users/github-users.ts
+++ app/providers/github-users/github-users.ts
@@ -37,5 +37,18 @@ export class GithubUsers {
         });
     });
   }
+  
+  //Get user details from the github api
+  loadDetails(login: string) {
+	  //get the data from the api and return it as a promise
+	  return new Promise<User>(resolve => {
+		  // Change the url to match https://api.github.com/users/{username}
+		  this.http.get(`https://api.github.com/users/${login}`)
+			.map(res => <User>(res.json()))
+			.subscribe(user => {
+				resolve(user);
+			});
+	  });
+  }
 }
 
