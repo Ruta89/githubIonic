a62043f8fdc0031c4d8c027b06256e1c432042eb
Arek Ruta <arek.ruta89@gmail.com>
2016-07-10 02:59:10 +0200

    dodanie menu

---
+++ app/pages/users/users.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the UsersPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/users/users.html',
+})
+export class UsersPage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
